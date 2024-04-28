6438112d60f8c6d033fd4ec2c5fac9106bb32b5b
Marcos Roberto Mazzero Junior <juninhomazzero@gmail.com>
2024-04-28 09:17:18 -0300

    Conexao com o Banco de Dados MySql

---
+++ ConnectBD.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+conn = mysql.connector.connect(
+  host = 'localhost',
+  user = 'root',
+  password = '1234',
+  database = 'Cadastro'
+)
+
+cursor = conn.cursor()
+
+
+
+
