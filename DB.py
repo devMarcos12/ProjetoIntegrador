f8fa9c22e75a17dc17daabea23b5764e4deddfc7
Marcos Roberto Mazzero Junior <juninhomazzero@gmail.com>
2024-05-07 13:59:17 -0300

    Fase 2 Concluída

--- DB.py
+++ DB.py
@@ -1,86 +1,105 @@
 from ConnectBD import conn
 
 cursor = conn.cursor()
+
+# Sub-Programas
+
+def cadastrar():
+  pass
  
+def listar_Produtos():
+  cursor.execute(f'SELECT * FROM cadastro.produto')
+  result = cursor.fetchall()
+  for row in result:
+    nameProduct = row[1]
+    CustoProduto = row[3]
+    PercentCF = row[4]
+    PercentCV = row[5]
+    PercentIV = row[6]
+    PercentML = row[7]
+    
+    MargemLucro = (CustoProduto/100) * PercentML 
+    # Cálculo Preço de Venda
+    PrecoVenda = CustoProduto / (1-((PercentCF+PercentCV+PercentIV+PercentML)/(100)))
+    PercentPV = 100
+    # Cálculo das Procentagem
+    PercentCp = PercentPV - PercentCF - PercentCV - PercentIV - PercentML
+    # Cálculo Receita Bruta
+    ReceitaBruta = PrecoVenda - CustoProduto
+    PercentRb = PercentPV - PercentCp
+    #transformando as porcentagens lidas nos respectivos valores
+    Valor_CustoFixo = (PrecoVenda / 100 ) * PercentCF
+    Valor_ComisaoVendas = (PrecoVenda / 100) * PercentCV
+    Valor_Imposto = (PrecoVenda / 100) * PercentIV
+    # Cálculo de Outros Custos
+    OutrosCustos = Valor_CustoFixo + Valor_ComisaoVendas + Valor_Imposto
+    PercentOutrosCustos = PercentCF + PercentCV + PercentIV
+    # Cálculo Rentabilidade
+    rent = ReceitaBruta - OutrosCustos
+    
+    print()
+    print(f"{'-'*25} | {'-'*10} | {'-'*5}") 
+    print (f"{'Descrição':^25} | {'Valor':^10} | {'%':^5}")
+    print(f"{'-'*25} | {'-'*10} | {'-'*5}")
+    
+    print (f"{'A. Preço de Venda':<25} | {f'{PrecoVenda:.2f}':^10} | {f'{PercentPV}%':>5}")
+    print (f"{'B. Custo de Aquisição':<25} | {f'{CustoProduto:.2f}':^10} | {f'{PercentCp:.0f}%':>5}")
+    print (f"{'C. Receita Bruta (A-B)':<25} | {f'{ReceitaBruta:.2f}':^10} | {f'{PercentRb:.0f}%':>5}")
+    print (f"{'D. Custo Fixo':<25} | {f'{Valor_CustoFixo:.2f}':^10} | {f'{PercentCF:.0f}%':>5}")
+    print (f"{'E. Comissão de Vendas':<25} | {f'{Valor_ComisaoVendas:.2f}':^10} | {f'{PercentCV:.0f}%':>5}")
+    print (f"{'F. Impostos':<25} | {f'{Valor_Imposto:.2f}':^10} | {f'{PercentIV:.0f}%':>5}")
+    print (f"{'G. Outros custos (D+E+F)':<25} | {f'{OutrosCustos:.2f}':^10} | {f'{PercentOutrosCustos:.0f}%':>5}")
+    print (f"{'H. Rentabilidade (C-G)':<25} | {f'{rent:.2f}':^10} | {f'{PercentML:.0f}%':>5}")
+    
+    print ("CLASSIFICAÇÃO DE LUCRO: ") 
+    if MargemLucro>20:
+        print(f'O lucro do {nameProduct} foi \033[34mAlto!\033[0m\n') #azul
+    elif MargemLucro >10 and MargemLucro<=20:
+        print(f'O lucro do {nameProduct} foi \033[32mMédio!\033[0m\n') #verde
+    elif MargemLucro >0 and MargemLucro<=10:
+        print(f'O lucro do {nameProduct} foi \033[33mBaixo!\033[0m\n') #amarelo
+    elif MargemLucro == 0:
+        print(f'O lucro do {nameProduct} foi Equilibrado!\n')
+    else:
+        print(f'O lucro do {nameProduct} foi \033[31mPrejuízo!\033[0m\n') #vermelho
+    print ()
+ 
+def atualizar():
+  pass
+
+def delete():
+  pass 
+
+ 
+# Menu do Programa
 done = False
 while not done:
-  print('1 - Create')
-  print('2 - Read')
-  print('3 - Update')
-  print('4 - Delete')
+  print('1. Cadastrar um Produto')
+  print('2. Atualizar um Produto.')
+  print('3. Deletar um Produto.')
+  print('4. Listar Todos os Produtos.')
+  print('5. Sair.')
   option = input('> ')
   
   if option == '1':
-    print('Opcao nao implementada')    
-  elif option == '2':
-    codigo_valido = False
-    while not codigo_valido:
-        try:
-            codigo_produto = int(input('Digite o código do produto: '))
-            if codigo_produto > 0:
-              codigo_valido = True
-            else:
-               print('Valor inválido! O código do produto deve ser maior que zero\n')
-        except ValueError:
-            print('Valor inválido! Digite somente números\n')
+    print('Opcao nao implementada')
+    cadastrar()    
     
-    cursor.execute(f'SELECT * FROM cadastro.produto WHERE codTenis = {codigo_produto}')
-    result = cursor.fetchall()
-    for row in result:
-      nameProduct = row[1]
-      CustoProduto = row[3]
-      PercentCF = row[4]
-      PercentCV = row[5]
-      PercentIV = row[6]
-      PercentML = row[7]
-      
-      CustoFixo = (CustoProduto/100) * PercentCF
-      ComissaoVendas = (CustoProduto/100) * PercentCV
-      Imposto = (CustoProduto/100) * PercentIV
-      MargemLucro = (CustoProduto/100) * PercentML 
-      # Cálculo Preço de Venda
-      PrecoVenda = CustoProduto / (1-((PercentCF+PercentCV+PercentIV+PercentML)/(100)))
-      PercentPV = 100
-      # Cálculo das Procentagem
-      PercentCp = PercentPV - PercentCF - PercentCV - PercentIV - PercentML
-      # Cálculo Receita Bruta
-      ReceitaBruta = PrecoVenda - CustoProduto
-      PercentRb = PercentPV - PercentCp
-      #transformando as porcentagens lidas nos respectivos valores
-      Valor_CustoFixo = (PrecoVenda / 100 ) * PercentCF
-      Valor_ComisaoVendas = (PrecoVenda / 100) * PercentCV
-      Valor_Imposto = (PrecoVenda / 100) * PercentIV
-      # Cálculo de Outros Custos
-      OutrosCustos = Valor_CustoFixo + Valor_ComisaoVendas + Valor_Imposto
-      PercentOutrosCustos = PercentCF + PercentCV + PercentIV
-      # Cálculo Rentabilidade
-      rent = ReceitaBruta - OutrosCustos
-
-      print('-'*50)
-      print('Descrição           /    Valor       /   %')
-      print(f'A. Preço de Venda:     / {PrecoVenda:.2f}   /  {PercentPV}')
-      print(f'B. Custo de Aquisição: / {CustoProduto:.2f} /  {PercentCp}%')
-      print(f'C. Receita Bruta:      / {ReceitaBruta:.2f} /  {PercentRb}%')
-      print(f'D. Custo Fixo:         / {Valor_CustoFixo:.2f}    /  {PercentCF}%')
-      print(f'E. Comissão de Venda:  / {Valor_ComisaoVendas:.2f}    /  {PercentCV}%')
-      print(f'F. Impostos:           / {Valor_Imposto:.2f}   /  {PercentIV}%')
-      print(f'G. Outros Custos:      / {OutrosCustos:.2f} /  {PercentOutrosCustos}%')
-      print(f'H. Rentabilidade:      / {rent:.2f}         /  {PercentML}% \n')
-      print('-'*50)
-      if MargemLucro > 20:
-        print(f'O lucro do {nameProduct} foi \33[42mAlto!\33[m\n')
-      elif MargemLucro > 10 and MargemLucro <= 20:
-        print(f'O lucro do {nameProduct} foi \33[43mMédio!\33[m\n')
-      elif MargemLucro > 0 and  MargemLucro <= 10:
-        print(f'O lucro do {nameProduct} foi \33[44mBaixo!\33[m\n')
-      elif MargemLucro == 0:
-        print(f'O lucro do {nameProduct} foi \33[45mEquilibrado!\33[m\n')
-      else:
-        print(f'O lucro do {nameProduct} foi \33[41mPrejuízo!\33[m\n')
-      
-  elif option == 3:
+  elif option == '2':
     print('Opcao nao implementada')
-  elif option == 4:
+    atualizar()
+    
+  elif option == '3':
     print('Opcao nao implementada')
+    delete()
+    
+  elif option == '4':
+    print('Opcao nao implementada')
+    listar_Produtos()
+    
+  elif option == '5':
+    print('Saindo do programa...')
+    break
+   
   else:
-    done = True
+    print('Digite uma opcao válida')
--- Fase1.py
+++ Fase1.py
@@ -1,114 +1,29 @@
-import os
+from DB import listar_Produtos, delete, atualizar, cadastrar
 
-def clear():
-  os.system('cls')
-
-try: 
-  nameProduct = input('Digite o nome do produto: ')
-  cod = int(input('Digite o código do Produto: '))
-  if cod <= 0:
-    print('Código inválido, o código deve ser maior do que 0!')
-  else:
-    CustoProduto = float(input('Qual o custo do Produto (porcentagem)? '))
-    if CustoProduto <= 0:
-      print('A porcentagem do Custo do Produto deve ser maior do que 0!')
-    else:
-      PercentCF = int(input('Qual o custo Fixo/Administrativo (porcentagem)? '))
-      if PercentCF <= 0:
-        print('A porcentagem do Custo Fixo/Administrativo deve ser maior do que 0!')
-      else:
-        PercentCV = int(input('Qual a Commissão de Vendas (CV)? '))
-        if PercentCV <= 0:
-          print('A porcentagem da Comissão de Vendas deve ser maior do que 0!')
-        else:
-          PercentIV = int(input('Qual o Imposto (IV)? '))
-          if PercentIV <= 0:
-            print('A porcentagem do Imposto deve ser maior do que 0!')
-          else: 
-            PercentML = int(input('Qual a Margem de lucro(ML)? '))
-            if PercentML <= 0:
-              print('A porcentagem da Margem de Lucro deve ser maior do que 0!')
-except ValueError:
-  print('O valor deve ser maior do que 0')
-
-# Calculando quantos reais as procentagens estão gerando em cima do Preço de Venda
-CustoFixo = (CustoProduto/100) * PercentCF
-ComissaoVendas = (CustoProduto/100) * PercentCV
-Imposto = (CustoProduto/100) * PercentIV
-MargemLucro = (CustoProduto/100) * PercentML 
-
-# Cálculo Preço de Venda
-PrecoVenda = CustoProduto / (1-((PercentCF+PercentCV+PercentIV+PercentML)/(100)))
-PercentPV = 100
-
-# Cálculo das Procentagem
-PercentCp = PercentPV - PercentCF - PercentCV - PercentIV - PercentML
-
-# Cálculo Receita Bruta
-ReceitaBruta = PrecoVenda - CustoProduto
-PercentRb = PercentPV - PercentCp
-
-#transformando as porcentagens lidas nos respectivos valores
-Valor_CustoFixo = (PrecoVenda / 100 ) * PercentCF
-Valor_ComisaoVendas = (PrecoVenda / 100) * PercentCV
-Valor_Imposto = (PrecoVenda / 100) * PercentIV
-
-# Cálculo de Outros Custos
-OutrosCustos = Valor_CustoFixo + Valor_ComisaoVendas + Valor_Imposto
-PercentOutrosCustos = PercentCF + PercentCV + PercentIV
-
-# Cálculo Rentabilidade
-rent = ReceitaBruta - OutrosCustos
-
-
-def exibirTabela():
-  print('-'*50)
-  print('Descrição           /    Valor       /   %')
-  print(f'A. Preço de Venda:     / {PrecoVenda:.2f}   /  {PercentPV}')
-  print(f'B. Custo de Aquisição: / {CustoProduto:.2f} /  {PercentCp}%')
-  print(f'C. Receita Bruta:      / {ReceitaBruta:.2f} /  {PercentRb}%')
-  print(f'D. Custo Fixo:         / {Valor_CustoFixo:.2f}    /  {PercentCF}%')
-  print(f'E. Comissão de Venda:  / {Valor_ComisaoVendas:.2f}    /  {PercentCV}%')
-  print(f'F. Impostos:           / {Valor_Imposto:.2f}   /  {PercentIV}%')
-  print(f'G. Outros Custos:      / {OutrosCustos:.2f} /  {PercentOutrosCustos}%')
-  print(f'H. Rentabilidade:      / {rent:.2f}         /  {PercentML}% \n')
-  print('-'*50)
-  
-def lucroRent():
-  if MargemLucro > 20:
-    print(f'O lucro do {nameProduct} foi \33[42mAlto!\33[m\n')
-  elif MargemLucro > 10 and MargemLucro <= 20:
-    print(f'O lucro do {nameProduct} foi \33[43mMédio!\33[m\n')
-  elif MargemLucro > 0 and  MargemLucro <= 10:
-    print(f'O lucro do {nameProduct} foi \33[44mBaixo!\33[m\n')
-  elif MargemLucro == 0:
-    print(f'O lucro do {nameProduct} foi \33[45mEquilibrado!\33[m\n')
-  else:
-    print(f'O lucro do {nameProduct} foi \33[41mPrejuízo!\33[m\n')
-    
-  
 done = False
-
 while not done:
-  try:
-    print('Qual opção deseja?')
-    print('1 - Descrição, Valor e Porcentagem.')
-    print('2 -  Qual a classificação do Lucro que o produto está.')
-    print('3 - Sair.')
-    choice = input('> ')
-    clear()
-
-    if choice == '1':
-      clear()
-      exibirTabela()
-    elif choice == '2':
-      clear()
-      lucroRent()
-    elif choice == '3':
-      done = True
-    else:
-      clear()
-      print('Digite uma opção entre 1 e 3')
-  except ValueError:
-    clear()
-    print('Somente números de 1 a 3')
+  print('1. Cadastrar um Produto')
+  print('2. Atualizar um Produto.')
+  print('3. Deletar um Produto.')
+  print('4. Listar Todos os Produtos.')
+  print('5. Sair.')
+  option = input('> ')
+  
+  if option == '1':
+    cadastrar()    
+    
+  elif option == '2':
+    atualizar()
+    
+  elif option == '3':
+    delete()
+    
+  elif option == '4':
+    listar_Produtos() # Funcao implementada
+    
+  elif option == '5':
+    print('Saindo do programa...')
+    break
+   
+  else:
+    print('Digite uma opcao válida.')
